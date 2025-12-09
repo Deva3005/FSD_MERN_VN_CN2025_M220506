74844adaa664dcf322e5cbb0e674595a46b330dc
Deva <deva@msigrey.field>
2025-12-09 10:59:18 +0530

    Assignment 04

---
+++ Assignments/Assignment04/CollegePortal/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img
+        src="https://server.wallpaperalchemy.com/storage/wallpapers/92/windows-xp-wallpaper-bliss-4k-wallpaper.jpeg"
+        alt="Website Under Development"
+      />
+    </>
+  );
+}
+
+export default App;
